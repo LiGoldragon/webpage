14ed5438e157a9d65b4bd15e47dd31c3d89a2444
li <li@maisiliym.criome.net>
2025-12-02 17:15:30 +0100

    autofmt(flake.nix)

--- flake.nix
+++ flake.nix
@@ -6,40 +6,59 @@
     papermod.url = "github:adityatelange/hugo-PaperMod";
   };
 
-  outputs = { self, nixpkgs, papermod }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      papermod,
+    }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    in {
-      packages = forAllSystems (system: let
-        pkgs = import nixpkgs { inherit system; };
-        theme = papermod;
-      in {
-        site = pkgs.stdenv.mkDerivation {
-          pname = "ligoldragon-site";
-          version = "1.0.0";
-          src = self;
-          nativeBuildInputs = [ pkgs.hugo ];
-          buildPhase = ''
-            mkdir -p themes
-            ln -s ${theme} themes/PaperMod
-            hugo --minify --destination $out
-          '';
-          installPhase = "";
-        };
-      });
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          theme = papermod;
+        in
+        {
+          site = pkgs.stdenv.mkDerivation {
+            pname = "ligoldragon-site";
+            version = "1.0.0";
+            src = self;
+            nativeBuildInputs = [ pkgs.hugo ];
+            buildPhase = ''
+              mkdir -p themes
+              ln -s ${theme} themes/PaperMod
+              hugo --minify --destination $out
+            '';
+            installPhase = "";
+          };
+        }
+      );
 
-      devShells = forAllSystems (system: let
-        pkgs = import nixpkgs { inherit system; };
-        theme = papermod;
-      in {
-        default = pkgs.mkShell {
-          buildInputs = [ pkgs.hugo ];
-          shellHook = ''
-            mkdir -p themes
-            ln -sfn ${theme} themes/PaperMod
-            echo "PaperMod theme available at themes/PaperMod (from nix input)."
-          '';
-        };
-      });
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          theme = papermod;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ pkgs.hugo ];
+            shellHook = ''
+              mkdir -p themes
+              ln -sfn ${theme} themes/PaperMod
+              echo "PaperMod theme available at themes/PaperMod (from nix input)."
+            '';
+          };
+        }
+      );
     };
 }
