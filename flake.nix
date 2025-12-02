bf640fcd0ea2d5a050dd56630308fa59aa1385b0
Olivier Francoeur <106389965+LiGoldragon@users.noreply.github.com>
2025-12-02 16:56:02 +0100

    Use nix flake for PaperMod theme

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Static site for Li Goldragon built with Hugo and PaperMod";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    papermod.url = "github:adityatelange/hugo-PaperMod";
+  };
+
+  outputs = { self, nixpkgs, papermod }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in {
+      packages = forAllSystems (system: let
+        pkgs = import nixpkgs { inherit system; };
+        theme = papermod;
+      in {
+        site = pkgs.stdenv.mkDerivation {
+          pname = "ligoldragon-site";
+          version = "1.0.0";
+          src = self;
+          nativeBuildInputs = [ pkgs.hugo ];
+          buildPhase = ''
+            mkdir -p themes
+            ln -s ${theme} themes/PaperMod
+            hugo --minify --destination $out
+          '';
+          installPhase = "";
+        };
+      });
+
+      devShells = forAllSystems (system: let
+        pkgs = import nixpkgs { inherit system; };
+        theme = papermod;
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.hugo ];
+          shellHook = ''
+            mkdir -p themes
+            ln -sfn ${theme} themes/PaperMod
+            echo "PaperMod theme available at themes/PaperMod (from nix input)."
+          '';
+        };
+      });
+    };
+}
